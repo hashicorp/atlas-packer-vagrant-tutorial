107ac4767dcd0e1a875b5a6a2a6612755ffbeb38
Raimondas Rimkevičius <github@mekdrop.name>
2015-06-29 14:24:19 +0300

    some debug messages

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -10,6 +10,7 @@ chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 
 # Configure sudoers
+echo "Configure sudoers..."
 mv /etc/sudoers /etc/sudoers.old
 mv /tmp/data/etc/sudoers /etc/
 chmod 0440 /etc/sudoers
@@ -17,4 +18,5 @@ chown root /etc/sudoers
 chgrp root /etc/sudoers
 
 # Customize the message of the day
+echo "Customize the message of the day..."
 echo 'ImpressCMS Development Environment' > /etc/motd
