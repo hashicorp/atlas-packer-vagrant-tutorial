6e32c5ee658d54e3a2317b7f2fc4df8d0a18bb1e
Raimondas Rimkevičius <github@mekdrop.name>
2015-06-30 20:35:08 +0300

    fix ?

--- scripts/fix-hyperv.sh
+++ scripts/fix-hyperv.sh
@@ -4,10 +4,10 @@
 #apt-get install hv-kvp-daemon-init
 #cp /usr/lib/linux-tools/3.19.0-21/hv_* /usr/sbin/
 
-sudo echo "hv_vmbus" >> /etc/initramfs-tools/modules
-sudo echo "hv_storvsc" >> /etc/initramfs-tools/modules
-sudo echo "hv_blkvsc" >> /etc/initramfs-tools/modules
-sudo echo "hv_netvsc" >> /etc/initramfs-tools/modules
+sudo 'echo "hv_vmbus" >> /etc/initramfs-tools/modules'
+sudo 'echo "hv_storvsc" >> /etc/initramfs-tools/modules'
+sudo 'echo "hv_blkvsc" >> /etc/initramfs-tools/modules'
+sudo 'echo "hv_netvsc" >> /etc/initramfs-tools/modules'
 
 sudo update-initramfs –u -k all
 
