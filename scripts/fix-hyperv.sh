77e121a3c577ac56b17d76ee847fe03e2f2531e9
Raimondas Rimkevičius <github@mekdrop.name>
2015-06-30 21:55:29 +0300

    added spaces

--- scripts/fix-hyperv.sh
+++ scripts/fix-hyperv.sh
@@ -9,23 +9,23 @@ echo "hv_storvsc" | sudo tee --append /etc/initramfs-tools/modules
 echo "hv_blkvsc" | sudo tee --append /etc/initramfs-tools/modules 
 echo "hv_netvsc" | sudo tee --append /etc/initramfs-tools/modules 
 
-sudo `echo "update-initramfs -u"`
+sudo `echo "update-initramfs -u"` 
 
-sudo apt-get -y install linux-cloud-tools-common linux-cloud-tools-virtual
-sudo cp /usr/lib/linux-tools/`uname -r`/hv_* /usr/sbin/
+sudo apt-get -y install linux-cloud-tools-common linux-cloud-tools-virtual 
+sudo cp /usr/lib/linux-tools/`uname -r`/hv_* /usr/sbin/ 
 
-sudo /opt/VBoxGuestAdditions-*/uninstall.sh
-rm -rf /usr/lib/VBoxGuestAdditions
+sudo /opt/VBoxGuestAdditions-*/uninstall.sh 
+rm -rf /usr/lib/VBoxGuestAdditions 
 
 rm -rf /home/vagrant/.ssh
 mkdir -pm 700 /home/vagrant/.ssh
-lynx --dump https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub > /home/vagrant/.ssh/authorized_keys
-chmod 0600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant /home/vagrant/.ssh
+lynx --dump https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub > /home/vagrant/.ssh/authorized_keys 
+chmod 0600 /home/vagrant/.ssh/authorized_keys 
+chown -R vagrant /home/vagrant/.ssh 
 
 echo "Done."
 echo "Now run build.bat again to finish everything"
 
-#history -c && history -w
+#history -c && history -w 
 
 #exit
