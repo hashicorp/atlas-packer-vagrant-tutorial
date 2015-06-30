7b081bb99ba604040656ce3a2b7c8f5f5c61a2fe
Raimondas Rimkevičius <github@mekdrop.name>
2015-06-30 22:10:46 +0300

    possible strange chars fix ?

--- scripts/fix-hyperv.sh
+++ scripts/fix-hyperv.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 
-#apt-get install linux-tools-3.19.0-21-generic
-#apt-get install hv-kvp-daemon-init
-#cp /usr/lib/linux-tools/3.19.0-21/hv_* /usr/sbin/
-
 echo "hv_vmbus" | sudo tee --append /etc/initramfs-tools/modules 
 echo "hv_storvsc" | sudo tee --append /etc/initramfs-tools/modules 
 echo "hv_blkvsc" | sudo tee --append /etc/initramfs-tools/modules 
 echo "hv_netvsc" | sudo tee --append /etc/initramfs-tools/modules 
 
-sudo `echo "update-initramfs -u"` 
+sudo `echo "update-initramfs -u"`
 
-sudo apt-get -y install linux-cloud-tools-common linux-cloud-tools-virtual 
-sudo cp /usr/lib/linux-tools/`uname -r`/hv_* /usr/sbin/ 
+sudo apt-get -y install linux-cloud-tools-common linux-cloud-tools-virtual
+sudo cp /usr/lib/linux-tools/`uname -r`/hv_* /usr/sbin/
 
-sudo /opt/VBoxGuestAdditions-*/uninstall.sh 
+sudo /opt/VBoxGuestAdditions-*/uninstall.sh
 rm -rf /usr/lib/VBoxGuestAdditions 
 
-rm -rf /home/vagrant/.ssh 
-mkdir -pm 700 /home/vagrant/.ssh 
-lynx --dump https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub > /home/vagrant/.ssh/authorized_keys 
-chmod 0600 /home/vagrant/.ssh/authorized_keys 
-chown -R vagrant /home/vagrant/.ssh 
+touch /tmp/1.sh
+echo 'rm -rf /home/vagrant/.ssh' >> /tmp/1.sh
+echo "mkdir -pm 700 /home/vagrant/.ssh" >> /tmp/1.sh
+echo "wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys" >> /tmp/1.sh
+echo "chmod 0600 /home/vagrant/.ssh/authorized_keys" >> /tmp/1.sh
+echo "chown -R vagrant /home/vagrant/.ssh" >> /tmp/1.sh
+chmod 777 /tmp/1.sh
+/tmp/1.sh
+rm -rf /tmp/1.sh
 
 echo "Done."
 echo "Now run build.bat again to finish everything"
 
-##history -c && history -w 
-
-#exit
+#history -c && history -w
