4fab1d97eedb9db4c2a1af659e7d2594c0651775
Raimondas Rimkevičius <github@mekdrop.name>
2015-12-27 12:03:08 +0200

    removed apt-get replacing

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-perl -p -i -e 's#http://us.archive.ubuntu.com/ubuntu#mirror://mirrors.ubuntu.com/mirrors.txt#gi' /etc/apt/sources.list
-
 # Update the box
 apt-get -y update >/dev/null
 apt-get -y install facter linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip >/dev/null
