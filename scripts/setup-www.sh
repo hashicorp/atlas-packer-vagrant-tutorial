fc0ff1f17e04988db6d32d352f585529684c2e77
Raimondas Rimkevičius <github@mekdrop.name>
2016-04-22 15:46:20 +0300

    Update setup-www.sh

--- scripts/setup-www.sh
+++ scripts/setup-www.sh
@@ -46,7 +46,7 @@ chgrp -R www-data impresscms
 rar a -m5 -md512m -ow -r -s -t -tk -y -rr -k -ep1 impresscms.rar impresscms/
 rm -rf impresscms
 
-(crontab -l ; echo "@reboot sleep 20s && [ ! -d /vagrant/www ] && touch /tmp/WWW_EXTRACTING && mkdir -p /vagrant/www && cd /vagrant/www && unrar x -y -o- /srv/backup/impresscms.rar && rm -rf /tmp/WWW_EXTRACTING") | crontab
+(crontab -l ; echo "@reboot sleep 20s && screen -d -m /usr/local/bin/icms-extract-backup.sh") | crontab
 
 echo "Setuping Memcached..."
 cd /srv/www
