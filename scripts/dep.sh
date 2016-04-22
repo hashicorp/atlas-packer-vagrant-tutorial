2fa430a3c66da58464af7d2625929f9d06c1d87a
Raimondas Rimkevičius <github@mekdrop.name>
2016-04-22 16:40:38 +0300

    Update dep.sh

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -15,10 +15,10 @@ echo "Installing MySQL server, client and some tools..."
 apt-get -q -y install mysql-server mysql-client mysqltuner memcached
 
 echo "Installing web default dependencies..."
-apt-get -q -y install apache2 php5 php5-cli php5-gd mysql-client php5-mysqlnd php5-xdebug php5-memcached php5-mcrypt php5-curl php5-json
+apt-get -q -y install apache2 php php-cli php-gd mysql-client php-mysqlnd php-xdebug php-memcached php-mcrypt php-curl php-json
 
 echo "Installing some extra PHP libs..."
-apt-get -q -y install php-codesniffer php5-imagick php-pear php5-memcache php5-sqlite php5-mongo
+apt-get -q -y install php-codesniffer php-imagick php-pear php-memcache php-sqlite php-mongo
 
 echo "Installing E-mail support..."
 apt-get -q -y install sendmail
@@ -26,7 +26,7 @@ apt-get -q -y install sendmail
 echo "Install PHPUnit..."
 wget https://phar.phpunit.de/phpunit.phar
 chmod +x phpunit.phar
-sudo mv phpunit.phar /usr/local/bin/phpunit
+mv phpunit.phar /usr/local/bin/phpunit
 phpunit --version
 
 echo "Installing BlackFire.IO agent..."
@@ -46,7 +46,7 @@ chmod +x /usr/local/bin/icms-extract-backup.sh
 
 echo "Installing phpMyAdmin..."
 apt-get -q -y install phpmyadmin
-php5enmod mcrypt
+phpenmod mcrypt
 rm -rf /etc/phpmyadmin/config.inc.php
 cp /tmp/data/web-config/phpmyadmin/config.inc.php /etc/phpmyadmin/config.inc.php
 ln -s /etc/phpmyadmin/apache.conf /etc/apache2/mods-enabled/phpmyadmin.conf
