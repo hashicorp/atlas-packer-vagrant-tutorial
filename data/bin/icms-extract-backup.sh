c0897289e9aac4900e0eaa9a49a3a38ca7ddc6f3
Raimondas Rimkevičius <github@mekdrop.name>
2016-04-22 18:38:13 +0300

    Update icms-extract-backup.sh

--- data/bin/icms-extract-backup.sh
+++ data/bin/icms-extract-backup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 if [ ! -d /vagrant/www ]; then
+  echo "Waiting..."
+  sleep 40s
+  echo "Doing job..."
   touch /tmp/WWW_EXTRACTING 
   mkdir -p /vagrant/www 
   cd /vagrant/www 
@@ -8,4 +11,5 @@ if [ ! -d /vagrant/www ]; then
   cd /vagrant/www/htdocs
   composer update --no-interaction
   rm -rf /tmp/WWW_EXTRACTING
+  echo "Done."
 fi
