96bceafd1bb3eab2e9a6f26c0ff1063c65cc70f3
Raimondas Rimkevičius <github@mekdrop.name>
2016-04-24 19:40:30 +0300

    now system services use systemd

--- data/bin/icms-extract-backup.sh
+++ data/bin/icms-extract-backup.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
-if [ ! -d /vagrant/www ]; then
-  echo "Waiting..."
-  sleep 40s
-  echo "Doing job..."
-  touch /tmp/WWW_EXTRACTING 
-  mkdir -p /vagrant/www 
-  cd /vagrant/www 
-  unrar x -y -o- /srv/backup/impresscms.rar
-  cd /vagrant/www/htdocs
-  composer update --no-interaction
-  rm -rf /tmp/WWW_EXTRACTING
-  echo "Done."
-fi
+if [ "$1" == "stop" ]; then
+	pgrep $0 | while read -r pid ; do
+	    kill $pid
+	done
+	exit 0;
+fi;
+
+if [ "$1" == "bg" ]; then
+	nohup $0 bg 0<&- &> /var/log/icms.log &
+	exit 0;
+fi;
+
+is_running=0
+while true; do
+
+	sleep 20s
+
+	if [ ! -d /vagrant/www ]; then
+	  if [ "$is_running" == "0" ]; then
+		  is_running=1
+		  echo "Doing job..."
+		  touch /vagrant/WWW_EXTRACTING 
+		  mkdir -p /vagrant/.vagrant/www
+		  cd /vagrant/.vagrant/www 
+		  unrar x -y -o- /srv/backup/impresscms.rar
+		  cd /vagrant/.vagrant/www/htdocs
+		  composer update --no-interaction
+ 		  rm -rf /vagrant/www
+		  mv /vagrant/.vagrant/www /vagrant/www
+		  rm -rf /vagrant/WWW_EXTRACTING
+		  echo "Done."
+		  is_running=0
+	  fi
+	fi
+
+done
